034e18ee1dbdb99c08855ee6138ff91c9b3bb71c
Jennyzzhh <zzh19980929@gmail.com>
2023-04-29 00:52:56 -0400

    fix thought controller issue

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,7 +4,7 @@ module.exports = {
     getThoughts(req, res) {
         Thought.find()
             .then((thoughts) => res.json(thoughts))
-            .catch((err) => res.status(500)).json(err);
+            .catch((err) => res.status(500).json(err));
     },
     getSingleThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
